15550074947be804bbb7d23c700f7a7442674546
CarlosOlea <carlos.olea.reyes@gmail.com>
2022-07-27 15:37:35 -0500

    added provider

--- versions.tf
+++ versions.tf
@@ -1,13 +1,15 @@
 terraform {
-
- 
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.23.0"
     }
   }
-
   required_version = ">= 0.14.0"
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+}
+
 }
