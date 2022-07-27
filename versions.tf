90af82634f8853074c06ec9dbfecb97c35ad1a39
CarlosOlea <carlos.olea.reyes@gmail.com>
2022-07-27 16:47:56 -0500

    using env aws cred

--- versions.tf
+++ versions.tf
@@ -11,6 +11,6 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
   //profile = "default"
-  access_key = "AKIAQFOK4JNRV46VPH53"
-  secret_key = "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"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
 }
